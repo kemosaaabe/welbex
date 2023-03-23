12c5b373f7f467cdf81c8083e1dc70a20a4b2e79
kemosaaabe <sanchellos27@mail.ru>
2023-03-23 23:23:44 +0300

    [+] styles Footer [+] balls

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,7 +1,99 @@
 import styles from './Footer.module.scss';
 
 const Footer = () => {
-    return <footer className={styles.footer}>hello from footer</footer>;
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footerRow}>
+                <div className={styles.column}>
+                    <h5>О компании</h5>
+                    <ul>
+                        <li>
+                            <a href="/">Партнёрская программа</a>
+                        </li>
+                        <li>
+                            <a href="/">Вакансии</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    <h5>Меню</h5>
+                    <div className={styles.menu}>
+                        <ul>
+                            <li>
+                                <a href="/">Расчёт стоимости</a>
+                            </li>
+                            <li>
+                                <a href="/">Услуги</a>
+                            </li>
+                            <li>
+                                <a href="/">Виджеты</a>
+                            </li>
+                            <li>
+                                <a href="/">Интеграции</a>
+                            </li>
+                            <li>
+                                <a href="/">Наши клиенты</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <a href="/">Кейсы</a>
+                            </li>
+                            <li>
+                                <a href="/">Благодарственные письма</a>
+                            </li>
+                            <li>
+                                <a href="/">Сертификаты</a>
+                            </li>
+                            <li>
+                                <a href="/">Блог на Youtube</a>
+                            </li>
+                            <li>
+                                <a href="/">Вопрос / Ответ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.column}>
+                    <h5>Контакты</h5>
+                    <div className={styles.contacts}>
+                        <div className={styles.phoneNumber}>
+                            +7 555 555-55-55
+                        </div>
+                        <div className={styles.icons}>
+                            <a href="/">
+                                <img
+                                    src="/assets/img/headerIcons/telegram.svg"
+                                    alt="telegram"
+                                />
+                            </a>
+                            <a href="/">
+                                <img
+                                    src="/assets/img/headerIcons/viber.svg"
+                                    alt="viber"
+                                />
+                            </a>
+                            <a href="/">
+                                <img
+                                    src="/assets/img/headerIcons/whatsapp.svg"
+                                    alt="whatsApp"
+                                />
+                            </a>
+                        </div>
+                        <div className={styles.address}>
+                            Москва, Путевой проезд 3с1, к 902
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.copyRight}>
+                <p>©WELBEX 2022. Все права защищены.</p>
+                <p>
+                    <a href="/">Политика конфиденциальности</a>
+                </p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/layout/Main/index.js
+++ src/layout/Main/index.js
@@ -1,6 +1,90 @@
 import styles from './Main.module.scss';
 import Ball from '../../components/Ball';
 
+const Balls = () => {
+    return (
+        <>
+            <Ball
+                styles={{
+                    width: '259px',
+                    height: '259px',
+                    left: '602px',
+                    top: '-154px',
+                    background: '#833AB4',
+                    opacity: '0.5',
+                    filter: 'blur(196px)',
+                }}
+            />
+            <Ball
+                styles={{
+                    width: '311px',
+                    height: '311px',
+                    left: '-102px',
+                    top: '251px',
+                    background: '#961A1A',
+                    opacity: '0.5',
+                    filter: 'blur(196px)',
+                }}
+            />
+            <Ball
+                styles={{
+                    width: '83px',
+                    height: '83px',
+                    borderRadius: '50%',
+                    left: '34%',
+                    top: '8%',
+                    background: `radial-gradient(64% 64% at 27.25% 26.5%, rgba(255, 208, 208, 0.536) 0%, 
+                        rgba(212, 93, 93, 0.264) 59.67%, rgba(167, 49, 49, 0.248) 78.59%, 
+                        rgba(130, 25, 25, 0.248) 100%)`,
+                    boxShadow: '3px 6px 27px -10px rgba(223, 125, 125, 0.25)',
+                }}
+            />
+            <Ball
+                styles={{
+                    width: '60px',
+                    height: '60px',
+                    borderRadius: '50%',
+                    left: '60%',
+                    top: '-1%',
+                    background: `radial-gradient(64% 64% at 27.25% 26.5%, rgba(236, 208, 255, 0.64) 0%, 
+                                rgba(161, 93, 207, 0.24) 59.67%, rgba(118, 49, 164, 0.208) 78.59%, 
+                                rgba(86, 24, 128, 0.232) 100%)`,
+                    boxShadow: '3px 6px 27px -10px rgba(177, 118, 217, 0.25)',
+                    filter: 'blur(5px)',
+                }}
+            />
+            <Ball
+                styles={{
+                    width: '60px',
+                    height: '60px',
+                    borderRadius: '50%',
+                    left: '60%',
+                    top: '-1%',
+                    background: `radial-gradient(64% 64% at 27.25% 26.5%, rgba(236, 208, 255, 0.64) 0%, 
+                                rgba(161, 93, 207, 0.24) 59.67%, rgba(118, 49, 164, 0.208) 78.59%, 
+                                rgba(86, 24, 128, 0.232) 100%)`,
+                    boxShadow: '3px 6px 27px -10px rgba(177, 118, 217, 0.25)',
+                    filter: 'blur(5px)',
+                }}
+            />
+            <Ball
+                styles={{
+                    width: '28px',
+                    height: '28px',
+                    borderRadius: '50%',
+                    left: '60%',
+                    top: '55%',
+                    background: `radial-gradient(64% 64% at 27.25% 26.5%, rgba(255, 208, 208, 0.48) 0%, 
+                                rgba(212, 93, 93, 0.248) 59.67%, 
+                                rgba(167, 49, 49, 0.264) 78.59%, rgba(130, 25, 25, 0.232) 100%)`,
+                    boxShadow: '3px 6px 27px -10px rgba(223, 125, 125, 0.25)',
+                    filter: 'blur(2.5px)',
+                }}
+            />
+        </>
+    );
+};
+
 const Main = () => {
     return (
         <section className={styles.mainSection}>
@@ -41,28 +125,7 @@ const Main = () => {
                     <a href="/">Получить консультацию</a>
                 </div>
             </div>
-            <Ball
-                styles={{
-                    width: '259px',
-                    height: '259px',
-                    left: '602px',
-                    top: '-154px',
-                    background: '#833AB4',
-                    opacity: '0.5',
-                    filter: 'blur(196px)',
-                }}
-            />
-            <Ball
-                styles={{
-                    width: '311px',
-                    height: '311px',
-                    left: '-102px',
-                    top: '251px',
-                    background: '#961A1A',
-                    opacity: '0.5',
-                    filter: 'blur(196px)',
-                }}
-            />
+            <Balls />
         </section>
     );
 };
