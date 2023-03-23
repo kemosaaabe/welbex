8568b20c2cdd15a60ec0c3bcefa717efab05c5d1
kemosaaabe <sanchellos27@mail.ru>
2023-03-23 20:45:46 +0300

    [+] header html layout

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,7 +1,55 @@
 import styles from './Header.module.scss';
 
 const Header = () => {
-    return <header className={styles.header}>hello from header</header>;
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img src="/assets/img/logo_welbex.svg" alt="logo welbex" />
+            </div>
+            <nav className={styles.nav}>
+                <ul className={styles.navList}>
+                    <li className={styles.navListItem}>
+                        <a href="/">Услуги</a>
+                    </li>
+                    <li className={styles.navListItem}>
+                        <a href="/">Виджеты</a>
+                    </li>
+                    <li className={styles.navListItem}>
+                        <a href="/">Интеграции</a>
+                    </li>
+                    <li className={styles.navListItem}>
+                        <a href="/">Кейсы</a>
+                    </li>
+                    <li className={styles.navListItem}>
+                        <a href="/">Сертификаты</a>
+                    </li>
+                </ul>
+            </nav>
+            <div className={styles.contacts}>
+                <div className={styles.phoneNumber}>+7 555 555-55-55</div>
+                <div className={styles.icons}>
+                    <a href="/">
+                        <img
+                            src="/assets/img/headerIcons/telegram.svg"
+                            alt="telegram"
+                        />
+                    </a>
+                    <a href="/">
+                        <img
+                            src="/assets/img/headerIcons/viber.svg"
+                            alt="viber"
+                        />
+                    </a>
+                    <a href="/">
+                        <img
+                            src="/assets/img/headerIcons/whatsapp.svg"
+                            alt="whatsApp"
+                        />
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
